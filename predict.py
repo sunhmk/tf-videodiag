65aa206eb28b9b4f59de9cf3d3900ea770c181ba
ganyu1 <45676178+ganyu1@users.noreply.github.com>
2019-12-30 11:42:14 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -18,47 +18,50 @@ class  train_model:
             init = tf.global_variables_initializer()
             sess.run(init)
 
-    def main(self, img_roots):
-        def read_img(img_path):
-            img = cv2.imread(img_path)
-            print(img_path)
-            img = cv2.resize(img, (480, 300))
-            return img
+    def read_img(self,img_path):
+        img = cv2.imread(img_path)
+        print(img_path)
+        img = cv2.resize(img, (480, 300))
+        return img
 
-        def write_xml(pic_name, prediction):
-            pic = '{}'.format(pic_name)
-            pre = '{}'.format(prediction)
-            dirs = './sample.xml'
-            if not os.path.exists(dirs):
-                root = ET.Element('videoNodes')
-                tree = ET.ElementTree(root)
-                ET.dump(root)
-                tree.write('sample.xml', encoding="utf-8", xml_declaration=True)
-            tree = ET.parse('./sample.xml')
-            root = tree.getroot()
-            video = ET.Element('videoNode')
-            name = ET.SubElement(video, 'videoId')
-            name.text = pic
-            normal = ET.SubElement(video, 'normal')
-            normal.text = pre
-            root.append(video)
+    def write_xml(self,pic_name, prediction):
+        pic = '{}'.format(pic_name)
+        pre = '{}'.format(prediction)
+        dirs = './sample.xml'
+        if not os.path.exists(dirs):
+            root = ET.Element('videoNodes')
+            tree = ET.ElementTree(root)
+            ET.dump(root)
             tree.write('sample.xml', encoding="utf-8", xml_declaration=True)
+        tree = ET.parse('./sample.xml')
+        root = tree.getroot()
+        video = ET.Element('videoNode')
+        name = ET.SubElement(video, 'videoId')
+        name.text = pic
+        normal = ET.SubElement(video, 'normal')
+        normal.text = pre
+        root.append(video)
+        tree.write('sample.xml', encoding="utf-8", xml_declaration=True)
 
-        files = os.listdir(img_roots)
-        for file in files:
-            if file[-4] == '_' and file[:-4]!='.jpg':
-                continue
-            img = read_img(os.path.join(img_roots, file))
-            img_pre = img[np.newaxis, :]
-            with tf.Session() as sess:
-                prediction = sess.run(self.output_tensor_name,
-                                      feed_dict={self.input_image_tensor: img_pre,
-                                                 self.input_is_training_tensor: False})
-            # if prediction==0:
-            #     cv2.imshow('img',img)
-            #     cv2.waitKey(0)
-            write_xml(file,prediction)
+
+def main(img_roots):
+    files = os.listdir(img_roots)
+    train = train_model()
+    for file in files:
+        if file[-4] == '_' and file[:-4]!='.jpg':
+            continue
+        img = train.read_img(os.path.join(img_roots, file))
+        # img = read_img(os.path.join(img_roots, file))
+        img_pre = img[np.newaxis, :]
+        with tf.Session() as sess:
+            prediction = sess.run(train.output_tensor_name,
+                                  feed_dict={train.input_image_tensor: img_pre,
+                                             train.input_is_training_tensor: False})
+        # prediction = freeze_graph_test(pb_path,img_pre)
+        # if prediction==0:
+        #     cv2.imshow('img',img)
+        #     cv2.waitKey(0)
+        train.write_xml(file,prediction)
 
 if __name__ == '__main__':
-    train = train_model()
-    train.main('./test')
+    main('./test')
