fd4d40ead235bee148f039797fa7eb926a108ea4
ganyu1 <45676178+ganyu1@users.noreply.github.com>
2019-12-31 16:56:39 +0800

    Update imageProcess.py

--- imageProcess.py
+++ imageProcess.py
@@ -3,10 +3,9 @@ import os
 from abormalPicDet import train_model
 import numpy as np
 import tensorflow as tf
-from shutil import copyfile
-
 
 class imageProcess(train_model):
+
     def __init__(self, root, files, training = False):
         train_model.__init__(self)
         self.root = root
@@ -18,13 +17,8 @@ class imageProcess(train_model):
         else:
             self.label = self.readLabel()
         self.prediction = self.predict_image()
-        self.fm = self.variance_of_laplacian()
 
     def predict_image(self):
-        # files = os.listdir(self.root)
-        # for file in files:
-        #     if file[-4] == '_' and file[:-4] != '.jpg':
-        #         continue
         img_pre = self.img[np.newaxis, :]
         with tf.Session() as sess:
             prediction = sess.run(self.output_tensor_name,
@@ -32,31 +26,15 @@ class imageProcess(train_model):
                                              self.input_is_training_tensor: False})
         return prediction
 
-    def variance_of_laplacian(self):
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        fm = cv2.Laplacian(gray, cv2.CV_64F).var()
-        return fm
-
-    def readLabel(self):
-        if self.root == '':
-            return 0
-        elif self.root =='v':
-            return 1
-
 def main():
-    root = 'D:\GoogleDownload/trafficDataset/OriginalNoise'
-    new_path = './pre'
+    root = './test'
     files = os.listdir(root)
     for file in files:
         if file[-4] == '_' and file[:-4] != '.jpg':
             continue
         img = imageProcess(root, file)
-        if img.prediction == 0:
-            print(img.fm)
-            img1 = cv2.putText(img.image, "{:.2f}".format(img.fm),
-                        (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-            cv2.imwrite('{}'.format(os.path.join(new_path, file)), img1)
-
+        print(img.name)
+        img.write_xml(file, img.prediction)
 
 if __name__ == '__main__':
     main()
