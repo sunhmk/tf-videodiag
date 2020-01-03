2e414b6350f01998433f66ff2b41fd1f78280bf5
ganyu1 <45676178+ganyu1@users.noreply.github.com>
2020-01-03 13:52:54 +0800

    Update and rename abormalPicDet.py to network_model.py

--- network_model.py
+++ network_model.py
@@ -4,19 +4,25 @@ import tensorflow as tf
 import os
 from xml.etree import ElementTree as ET
 
-class  train_model:
+class  Network_model:
     def __init__(self):
-        pb_path = './pb_model_user'
+        pb_path = './model/pb_model_user4.pb'
         with tf.gfile.FastGFile(pb_path, "rb") as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
-            self.input_image_tensor, self.input_is_training_tensor,self.output_tensor_name\
-                = tf.import_graph_def(
-                graph_def, return_elements=["input_img:0","is_training:0",'prediction:0']
+            self.input_image_tensor, self.input_is_training_tensor,\
+            self.output_tensor_name,self.output_tensor_value = tf.import_graph_def(
+                graph_def, return_elements=["input_img:0","is_training:0",'prediction:0','output_value:0']
             )
         with tf.Session() as sess:
             init = tf.global_variables_initializer()
             sess.run(init)
+
+    def read_img(self,img_path):
+        img = cv2.imread(img_path)
+        print(img_path)
+        img = cv2.resize(img, (480, 300))
+        return img
 
     def write_xml(self,pic_name, prediction):
         pic = '{}'.format(pic_name)
